1a524922bc1f84efea25791b050b9b2d11a34c39
Easy lua <staskuzmin1406@gmail.com>
2025-07-12 21:30:20 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,19 +1,30 @@
 local targetRemote = game:GetService("ReplicatedStorage").GameEvents:WaitForChild("ActivePetService")
 
--- Хук namecall'ов
 local oldNamecall
+local count = 0
+local lastLogTime = tick()
+
 oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
     if not checkcaller() then
         local method = getnamecallmethod()
         
         if method == "FireServer" and self == targetRemote then
-            warn("[MyRemoteEvent] FireServer вызван!")
-            local args = {...}
-            for i, v in ipairs(args) do
-                print("Аргумент " .. i .. ":", v)
+            count = count + 1
+            local now = tick()
+
+            -- логируем только раз в 1 секунду или первые 5 раз
+            if count <= 5 or now - lastLogTime > 1 then
+                warn("[ActivePetService] FireServer вызван!")
+                local args = {...}
+                for i, v in ipairs(args) do
+                    print("  Аргумент " .. i .. ":", v)
+                end
+                lastLogTime = now
             end
         end
     end
 
     return oldNamecall(self, ...)
 end)
+
+warn("✅ Хук установлен")
