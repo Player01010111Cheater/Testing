34f585ba78a189122d74cd0a4d0be5b070131aa2
Easy lua <staskuzmin1406@gmail.com>
2025-07-18 14:39:11 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Swiftz/UI-Library/refs/heads/main/Libraries/Sea%20Hub%20-%20Example.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Swiftz/UI-Library/refs/heads/main/Libraries/Linoria%20-%20Example.lua"))()
