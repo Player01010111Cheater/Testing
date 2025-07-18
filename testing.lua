10960cbfa6d4980939b630094de44b93cb441a0f
Easy lua <staskuzmin1406@gmail.com>
2025-07-18 14:34:30 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,1 @@
-loadstring(game:HtppGet("https://raw.githubusercontent.com/x2Swiftz/UI-Library/refs/heads/main/Libraries/FluentUI-Example.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2Swiftz/UI-Library/refs/heads/main/Libraries/Sea%20Hub%20-%20Example.lua"))()
