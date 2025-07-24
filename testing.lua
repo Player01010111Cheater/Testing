f26b8395033ab0c44134ea1d8eab19a798c8b252
Easy lua <staskuzmin1406@gmail.com>
2025-07-24 13:04:58 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,9 +1,33 @@
-local Mana = loadstring(game:HttpGet("https://pastebin.com/raw/inP2PXxe"))()
-local Win = Mana:Window("NEVERLOSE", {Theme = "blue"}) -- Устанавливаем тему "blue"
-local Autofarm = Win:Tab("AutoFarm")
-Autofarm:Toggle("Auto Farm", false, function(t)
-    print("Auto Farm is", t and "enabled" or "disabled")
+local Neverlose_Main = loadstring(game:HttpGet"https://github.com/Mana42138/Neverlose-UI/blob/main/Source.lua")()
+
+local Win = Neverlose_Main:Window({
+    Title = "NEVERLOSE",
+    CFG = "Neverlose",
+    Key = Enum.KeyCode.H,
+    External = {
+        KeySystem = true,
+        Key = {
+            "Test",
+            "Beta"
+        }
+    }
+})
+
+local TabSection1 = Win:TSection("Misc")
+local Main = TabSection1:Tab("Main")
+local MainSection = Main:Section("Main Section")
+local ConfigSection = Main:Section("Config")
+
+
+local ToggleVar = MainSection:Toggle("Toggle", function(t)
+    ValueToggle = t
 end)
-Autofarm:Slider("Walk Speed", 1, 500, 16, function(v)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+ToggleVar:Set(true) -- can be true or false
+
+local SmallTable = {"Mana64", "Lmao", "HVH"}
+local SelectConfigVar = Config:Dropdown("Select Config", SmallTable, function(t)
+    ValueDropdown = t
+    print(ValueDropdown)
 end)
+SelectConfigVar:Set("Mana64") -- any existing name in the table, e.g., "Mana64"
+SelectConfigVar:Refresh({"New Mana64", "Legit"}) -- Refresh the dropdown with new table values
