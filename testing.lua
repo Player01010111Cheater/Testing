b49865aa910bdd9d2e721f397462d1c46cf6d928
Easy lua <staskuzmin1406@gmail.com>
2025-07-26 15:30:08 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,32 +1,43 @@
--- ⚠️ Безопасная эмуляция обхода Kick() через __namecall
-local Players = game:GetService("Players")
-local lp = Players.LocalPlayer
+-- Создание GUI
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "Ptx",
+    Folder = "ada",
+    Size = UDim2.fromOffset(580, 350),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 200,
+    -- Background = "", -- rbxassetid only
+    -- BackgroundImageTransparency = 0.42,
+    HideSearchBar = false,
+    ScrollBarEnabled = true,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            -- Тут логика
+        end,
+    },
 
--- Подмена warn (для тишины)
-local realWarn = warn
-warn = function(...) end
+})
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "MyGui"
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
--- Хук на namecall для перехвата :Kick()
-local mt = getrawmetatable(game)
-setreadonly(mt, false)
+-- Создание кнопки
+local button = Instance.new("TextButton")
+button.Name = "CenterButton"
+button.Size = UDim2.new(0, 150, 0, 50) -- ширина 150px, высота 50px
+button.Position = UDim2.new(0.5, 80, 0.5, -25) -- немного правее центра (по X)
+button.AnchorPoint = Vector2.new(0.5, 0.5) -- центрирование по центру кнопки
+button.Text = "Click me for open"
+button.BackgroundColor3 = Color3.fromRGB(0, 170, 255)
+button.TextColor3 = Color3.fromRGB(255, 255, 255)
+button.Font = Enum.Font.SourceSans
+button.TextSize = 24
+button.Parent = ScreenGui
 
-local oldNamecall = mt.__namecall
-
-mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    if tostring(self) == tostring(lp) and method == "Kick" then
-        realWarn("[Кряк] Попытка кика была перехвачена (через :Kick).")
-        return -- ничего не делаем
-    end
-    return oldNamecall(self, ...)
+-- Обработка нажатия
+button.MouseButton1Click:Connect(function()
+	Window:Open()
 end)
-
-setreadonly(mt, true)
-
--- Загружаем античит (если нужно)
--- loadstring(game:HttpGet("URL_ТВОЕГО_АНТИЧИТА"))()
-print("Loading anticrack")
--- 4. Загружаем твой основной античит
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/anticracker_LUA/refs/heads/main/anticrack.lua"))()
-
-
