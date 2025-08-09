42aa2371d7b05cd54a37dde7f8df3d029bc2229a
Easy lua <staskuzmin1406@gmail.com>
2025-08-09 18:47:02 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,57 @@
---[[Obfuscated by CCObfuscator]];local function cdXYB() local = math.random(); return cdxyzABCDXYBCDX end;if true then local bcdxyzAB = string.char(112)..string.char(114)..string.char(105)..string.char(110)..string.char(116) else local = math.random() end;for _ = 1, math.random(1, 2) do local xcdxyzABCDX = 0 end;while false do local ABcdxyzABdxyzABCDX = 96 end;local bcdxyzAB = string.char(112)..string.char(114)..string.char(105)..string.char(110)..string.char(116) ;if true then print(bcdxyzAB) else local dx = math.random() end;print(bcdxyzAB);if true then local function bcdxyzABCDXYZdxyzABCD() print(string.char(116)..string.char(101)..string.char(115)..string.char(116)) end else local zAy = math.random() end;for _ = 1, math.random(1, 2) do local dxyzABCDXYZ = 0 end;local function bcdxyzABCDXYZdxyzABCD() print(string.char(116)..string.char(101)..string.char(115)..string.char(116)) end ;local yzABCDDxyzABCD = math.random() * 10 - 5;for _ = 1, math.random(1, 2) do local bcdxyzABCDX = 0 end;while false do local DXY = 81 end;bcdxyzABCDXYZdxyzABCD()
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local player = game.Players.LocalPlayer
+local function notify(title, text, icon, time)
+    WindUI:Notify({
+        Title = title,
+        Content = text,
+        Icon = icon,
+        Duration = time,
+    })
+end
+local success, info = pcall(function() return game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId) end)
+local placeName = info.Name
+if success then placeName = #placeName > 50 and string.sub(placeName, 1, 50) .. "..." or placeName end
+
+local window = WindUI:CreateWindow({
+    Title = placeName,
+    Icon = "gem",
+    Author = "Crystal Client v1.5.1 [Premium]",
+    Folder = "CrystalClient",
+    Size = UDim2.fromOffset(580, 350),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 200,
+    -- Background = "", -- rbxassetid only
+    -- BackgroundImageTransparency = 0.42,
+    HideSearchBar = false,
+    ScrollBarEnabled = true,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            -- Тут логика
+        end,
+    },
+
+})
+
+WindUI:SetFont("rbxassetid://12187365364")
+window:EditOpenButton({
+    Title = "Crystal DLC",
+    Icon = "gem",
+    CornerRadius = UDim.new(0,16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new( -- gradient
+        Color3.fromHex("1E4F59"), -- морской синий
+        Color3.fromHex("FFFFFF")  -- белый
+    ),
+    OnlyMobile = false,
+    Enabled = true,
+    Draggable = true,
+})
+window:DisableTopbarButtons({
+    "Fullscreen",
+})
+window:CreateTopbarButton("theme-switcher", "moon", function()
+    WindUI:SetTheme(WindUI:GetCurrentTheme() == "Dark" and "Light" or "Dark")
+end, 990)
