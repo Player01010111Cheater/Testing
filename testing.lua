0c95d9c52246b0cf156d5de141a71d4c8afae23c
Easy lua <staskuzmin1406@gmail.com>
2025-08-26 20:56:06 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,6 +1,8 @@
 
-for _, obj in ipairs(gethui():GetDescendants()) do
+for _, obj in ipairs(gethui():GetChildren()) do
     if obj.ClassName == "ScreenGui" then
         print(obj.Name)
+    else
+        print("not: " .. obj.Name)
     end
 end
