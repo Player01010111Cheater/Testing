d6639ab69c85be65d3e6cfb6e338ebdb09c1c6ca
Easy lua <staskuzmin1406@gmail.com>
2025-09-01 18:51:37 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,13 +1,3 @@
-
-for _, obj in ipairs(gethui():GetChildren()) do
-    if obj.ClassName == "ScreenGui" then
-        print(obj.Name)
-    else
-        print("not: " .. obj.Name)
-    end
-end
-
-
 local screen = Instance.new("ScreenGui", gethui())
 screen.Name = "WindUI"
 local textlabel = Instance.new("TextLabel", screen)
