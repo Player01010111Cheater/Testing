1c7b2fe94c8614abf25ae5f5f5e2e3afe29ca0ed
Easy lua <staskuzmin1406@gmail.com>
2025-09-03 22:05:12 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,11 +1,13 @@
-local blockedSites = {"httpbin", "ipinfo"}
+local blockedSites = {"httpbin", "ipinfo", "ip", "webhooks"}
 local reqfunc = (syn or http).request
 local hook = hookfunction(reqfunc, function(req)
+	print(req)
     local url = req.Url or req.url or ""
     url = url:lower()
     for _, site in ipairs(blockedSites) do
 		print(url)
         if string.find(url, site:lower()) then
+			print("founded....")
             return { Success = false, StatusCode = 403, Body = "Access denied" }
         end
     end
