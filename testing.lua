061272e346d8769c3fe0136f8ffdc6b6c786cf14
Easy lua <staskuzmin1406@gmail.com>
2025-09-03 22:23:06 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -2,13 +2,12 @@ local blockedSites = {"httpbin", "ipinfo", "ip", "webhooks"}
 print("Loaded Block Http.")
 local reqfunc = (syn or http).request
 local hook = hookfunction(reqfunc, function(req)
-	print(req)
     local url = req.Url or req.url or ""
     url = url:lower()
     for _, site in ipairs(blockedSites) do
 		print(url)
         if string.find(url, site:lower()) then
-			print("founded....")
+			print("Blocked Url: " .. url)
             return { Success = false, StatusCode = 403, Body = "Access denied" }
         end
     end
@@ -21,8 +20,8 @@ hookfunction(originalHttpGet, function(self, url, ...)
     local lowerUrl = tostring(url):lower()
     
     for _, site in ipairs(blockedSites) do
-		print(url)
         if string.find(lowerUrl, site:lower()) then
+			print("Blocked url: " .. url)
             return "Access denied" -- блокировка, можно вернуть любую заглушку
         end
     end
