c453988d6c6d4041bd1f9c6fcc962782e9a53969
Easy lua <staskuzmin1406@gmail.com>
2025-09-04 12:42:15 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,4 +1,4 @@
-local blockedSites = {"httpbin", "ipinfo", "ip"}
+local blockedSites = {"httpbin.", "ipinfo.", "ipify."}
 local library = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local function notify(text)
     library:Notify({
@@ -14,8 +14,7 @@ oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
 
     for _, site in pairs(blockedSites) do
-        local pattern = "%f[%a]" .. site:lower()
-        if string.find(url, pattern, 1, true) then
+        if string.find(url, site:lower(), 1, true) then
             warn("[BLOCKED REQUEST] " .. url)
             notify("[BLOCKED REQUEST] " .. url)
             return { Success = false, StatusCode = 403, Body = "Access denied" }
@@ -29,8 +28,7 @@ end))
 local oldHttpGet
 oldHttpGet = hookfunction(game.HttpGet, newcclosure(function (self, url, ...)
     for _, site in pairs(blockedSites) do
-        local pattern = "([^%w])" .. site:lower()
-        if string.find(site:lower(), pattern) or string.sub(site:lower(), 1, #site) == site then
+        if string.find(url, site:lower(), 1 , true) then
             warn("[BLOCKED HTTPGET] " .. url)
             notify("[BLOCKED HTTPGET] " .. url)
             return "Access denied"
