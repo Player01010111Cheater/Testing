ebdc2556c37c7fd8fb8888ab81b9a57673168c94
Easy lua <staskuzmin1406@gmail.com>
2025-09-04 16:16:32 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,16 +1,1 @@
-local oldRequestGet
-oldRequestGet = hookfunction(request, newcclosure(function (req)
-    local url = (req.Url or req.url or ""):lower()
-    if string.find(url, "https://neoxsoftworks.eu") then
-        print("[DEBUG] KeySystem Hooked")
-        return {
-            Success = true, -- исправлено
-            StatusCode = 200,
-            Body = game:GetService("HttpService"):JSONEncode({
-                valid = true,
-            })
-        }
-    end
-    return oldRequestGet(req)
-end))
 
