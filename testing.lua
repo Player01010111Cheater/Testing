a10a734de63b44424de2007414a03d21912608cc
Easy lua <staskuzmin1406@gmail.com>
2025-09-05 22:19:45 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,12 +1,13 @@
 local oldRequestGet
 oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
-    if string.find(url, "luarmor.net") and string.find(url, "check_key") then
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "work.ink") and string.find(url, "tokenValid") then
         return {
             Success = true,
             StatusCode = 200,
             Body = game:GetService("HttpService"):JSONEncode({
-                code = "KEY_VALID",
+                valid = true,
             })
         }
     end
