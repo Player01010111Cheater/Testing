3fde6ff768d899048dba0bd65b7eb0322607f668
Easy lua <staskuzmin1406@gmail.com>
2025-09-05 13:35:46 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,12 +1,12 @@
 local oldRequestGet
 oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
-    if string.find(url, "pandadevelopment.net") and string.find(url, "validate") then
+    if string.find(url, "luarmor.net") and string.find(url, "check_key") then
         return {
             Success = true,
             StatusCode = 200,
             Body = game:GetService("HttpService"):JSONEncode({
-                success = true,
+                code = "KEY_VALID",
             })
         }
     end
