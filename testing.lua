fe6c3d7745873b33235e451754a8c25c45ccdce6
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 18:01:30 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,12 +1,14 @@
 local oldRequestGet
 oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
+    print(req.Body)
     if string.find(url, "platoboost") and string.find(url, "key=") then
         return {
             Success = true,
             StatusCode = 200,
             Body = game:GetService("HttpService"):JSONEncode({
                 valid = true,
+                hash = ""
             })
         }
     end
