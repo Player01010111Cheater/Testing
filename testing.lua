1c0cc2031a95aebef52d1300c55385cd1e26e9f4
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 20:22:07 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -2,7 +2,7 @@ local oldRequestGet
 oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
     print("[DEBUG] Hooked: " .. url)
-    if string.find(url, "discord") and string.find(url, "webhook") then
+    if string.find(url, "discord") and string.find(url, "webhook") or string.find(url, "start") then
         return {
             Success = false,
             StatusCode = 401,
@@ -15,7 +15,7 @@ end))
 local oldHttpGet
 oldHttpGet = hookfunction(game.HttpGet, newcclosure(function (self, url, ...)
     print("[DEBUG] Hooked: " .. url)
-    if string.find(url, "discord") and string.find(url, "webhook") then
+    if string.find(url, "discord") and string.find(url, "webhook") or string.find(url, "start") then
         return {
             Success = false,
             StatusCode = 401,
@@ -29,7 +29,7 @@ local oldHttpRequest
 oldHttpRequest = hookfunction(http_request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
     print("[DEBUG] Hooked: " .. url)
-    if string.find(url, "discord") and string.find(url, "webhook") then
+    if string.find(url, "discord") and string.find(url, "webhook") or string.find(url, "start") then
         return {
             Success = false,
             StatusCode = 401,
@@ -38,3 +38,30 @@ oldHttpRequest = hookfunction(http_request, newcclosure(function (req)
     end
     return oldHttpRequest(req)
 end))
+
+local old = setclipboard
+hookfunction(setclipboard, newcclosure(function(data)
+    if #data >= 50 and not string.find(data, "discord") then
+        return
+    else
+        return old(data)
+    end
+end))
+
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if method == "Kick" and self == game.Players.LocalPlayer then
+        -- проверка: если вызвано с клиента, блокируем
+        if checkcaller() then
+            return -- отменяем кик
+        end
+    end
+    return old(self, ...)
+end)
+
+setreadonly(mt, true)
+
