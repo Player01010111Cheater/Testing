09c75a9f16ade3f9187c591dc58218074da741ab
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 16:45:33 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,40 +1,54 @@
 local HttpService = game:GetService("HttpService")
 
--- список целевых функций (только KRNL)
-local targets = {
-    ["request"] = request,
-    ["http_request"] = http_request,
-    ["krnl.request"] = krnl and krnl.request,
-    ["game.HttpGet"] = game.HttpGet,
-    ["game.HttpPost"] = game.HttpPost
-}
+-- список целевых функций (только реально существующие)
+local targets = {}
+
+if request then
+    targets.request = request
+end
+
+if http_request then
+    targets.http_request = http_request
+end
+
+if krnl and krnl.request then
+    targets.krnl_request = krnl.request
+end
+
+if game.HttpGet then
+    targets.HttpGet = game.HttpGet
+end
+
+if game.HttpPost then
+    targets.HttpPost = game.HttpPost
+end
 
 -- универсальный хук
 local function hookHttp(name, func)
-    if typeof(func) == "function" then
-        local old
-        old = hookfunction(func, newcclosure(function(self, urlOrReq, ...)
-            local url = typeof(urlOrReq) == "table" and (urlOrReq.Url or urlOrReq.url) or urlOrReq
-            url = tostring(url):lower()
+    local old
+    old = hookfunction(func, newcclosure(function(self, urlOrReq, ...)
+        local url = typeof(urlOrReq) == "table" and (urlOrReq.Url or urlOrReq.url) or urlOrReq
+        url = tostring(url):lower()
 
-            print(("[KRNL DEBUG][%s] Hooked: %s"):format(name, url))
+        print("[KRNL DEBUG][" .. name .. "] Hooked: " .. url)
 
-            -- фильтр по work.ink/tokenValid
-            if string.find(url, "work.ink") and string.find(url, "tokenvalid") then
-                return {
-                    Success = true,
-                    StatusCode = 200,
-                    Body = HttpService:JSONEncode({ valid = true })
-                }
-            end
+        -- подмена ответа для work.ink/tokenValid
+        if string.find(url, "work.ink") and string.find(url, "tokenvalid") then
+            return {
+                Success = true,
+                StatusCode = 200,
+                Body = HttpService:JSONEncode({ valid = true })
+            }
+        end
 
-            return old(self, urlOrReq, ...)
-        end)))
-    end
+        return old(self, urlOrReq, ...)
+    end))
 end
 
--- применяем хуки
+-- хукаем всё, что найдено
 for name, func in pairs(targets) do
-    hookHttp(name, func)
+    if typeof(func) == "function" then
+        hookHttp(name, func)
+    end
 end
 
