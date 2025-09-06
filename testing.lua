68f26f6a2064892f37a7da39377f18cce716e582
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 17:04:28 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,31 +1,50 @@
-local HttpService = game:GetService("HttpService")
-local targets = {}
---
-if request then targets.request = request end
-if http_request then targets.http_request = http_request end
-if krnl and krnl.request then targets.krnl_request = krnl.request end
-if game.HttpGet then targets.HttpGet = game.HttpGet end
-if game.HttpPost then targets.HttpPost = game.HttpPost end
-print("na err1ors")
-local function hookHttp(name, func)
-    local old 
-    old = hookfunction(func, newcclosure(function(self, urlOrReq, ...)
-        local url = typeof(urlOrReq) == "table" and (urlOrReq.Url or urlOrReq.url) or urlOrReq
-        url = tostring(url):lower()
-        print("[KRNL DEBUG]["..name.."] Hooked: "..url)
-
-        if string.find(url, "work.ink") and string.find(url, "tokenvalid") then
-            return { Success = true, StatusCode = 200, Body = HttpService:JSONEncode({ valid = true }) }
-        end
-
-        return old(self, urlOrReq, ...)
-    end))
-end
-
-for name, func in pairs(targets) do
-    if typeof(func) == "function" then
-        hookHttp(name, func)
+local oldRequestGet
+oldRequestGet = hookfunction(request, newcclosure(function (req)
+    local url = (req.Url or req.url or ""):lower()
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "work.ink") and string.find(url, "tokenValid") then
+        return {
+            Success = true,
+            StatusCode = 200,
+            Body = game:GetService("HttpService"):JSONEncode({
+                valid = true,
+            })
+        }
     end
-end
+    return oldRequestGet(req)
+end))
+
+local oldHttpGet
+oldHttpGet = hookfunction(game.HttpGet, newcclosure(function (self, url, ...)
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url:lower(), "work.ink") and string.find(url:lower(), "tokenValid") then
+        print(self)
+        print(url)
+        for _, v in pairs(...) do
+            if typeof(v) == "function" then
+                local info = debug.getupvalue(v, 1)
+                print(info)
+            end
+        end
+        return oldHttpGet(self, url, ...)
+    end
+    return oldHttpGet(self, url, ...)
+end))
+
+local oldHttpRequest
+oldHttpRequest = hookfunction(http_request, newcclosure(function (req)
+    local url = (req.Url or req.url or ""):lower()
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "work.ink") and string.find(url, "tokenValid") then
+        return {
+            Success = true,
+            StatusCode = 200,
+            Body = game:GetService("HttpService"):JSONEncode({
+                valid = true,
+            })
+        }
+    end
+    return oldHttpRequest(req)
+end))
 
 
