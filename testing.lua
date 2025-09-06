18dcc748ca87379b919f4470295e8eb6f1eae0dd
Easy lua <staskuzmin1406@gmail.com>
2025-09-06 19:57:44 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,19 +1,40 @@
 local oldRequestGet
 oldRequestGet = hookfunction(request, newcclosure(function (req)
     local url = (req.Url or req.url or ""):lower()
-    print(req.Body)
-    if string.find(url, "platoboost") and string.find(url, "key=") then
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "discord") and string.find(url, "webhook") then
         return {
-            Success = true,
-            StatusCode = 200,
-            Body = game:GetService("HttpService"):JSONEncode({
-                success = true,
-                data = {
-                    valid = true,
-                    hash = "82dha72jda72"
-                }
-            })
+            Success = false,
+            StatusCode = 401,
+            Body = "PIDORAS FUCK YOU."
         }
     end
     return oldRequestGet(req)
+end))
+
+local oldHttpGet
+oldHttpGet = hookfunction(game.HttpGet, newcclosure(function (self, url, ...)
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "discord") and string.find(url, "webhook") then
+        return {
+            Success = false,
+            StatusCode = 401,
+            Body = "PIDORAS FUCK YOU."
+        }
+    end
+    return oldHttpGet(self, url, ...)
+end))
+
+local oldHttpRequest
+oldHttpRequest = hookfunction(http_request, newcclosure(function (req)
+    local url = (req.Url or req.url or ""):lower()
+    print("[DEBUG] Hooked: " .. url)
+    if string.find(url, "discord") and string.find(url, "webhook") then
+        return {
+            Success = false,
+            StatusCode = 401,
+            Body = "PIDORAS FUCK YOU."
+        }
+    end
+    return oldHttpRequest(req)
 end))
