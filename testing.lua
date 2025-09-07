bf5e8b1fcc6b3733bf7538d8611499e92225bbfe
Easy lua <staskuzmin1406@gmail.com>
2025-09-07 21:01:39 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,4 @@
-
+local utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/AternosCracker/refs/heads/main/utils/randomUtils.lua"))()
+print(utils.randomHash())
+print(utils.randomHash())
+print(utils.randomHash())
