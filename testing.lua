3132884b0858ca156d8ffbcd816e27a549bbffd1
Easy lua <staskuzmin1406@gmail.com>
2025-09-07 15:41:50 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,3 +1,29 @@
+-- Сохраняем оригинальные функции
+local Original = {
+    Kick = Players.LocalPlayer.Kick,
+    Shutdown = game.Shutdown,
+    SetClipboard = setclipboard
+}
+
+-- Переопределяем в глобальном окружении
+getgenv().Kick = function(...)
+    return Original.Kick(...)
+end
+
+getgenv().Shutdown = function(...)
+    return Original.Shutdown(...)
+end
+
+getgenv().setclipboard = function(...)
+    return Original.SetClipboard(...)
+end
+
+-- Защита от переопределения
+make_writeable_protected(nil) -- Если такая функция есть в вашем экзекьюторе
+
+
+
+
 hookfunction(game.Shutdown, function(...)
     print(...)
     return ...
