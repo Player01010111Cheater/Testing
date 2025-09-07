82aa498a9cce5973509d6e84ebf8bece6fb4423a
Easy lua <staskuzmin1406@gmail.com>
2025-09-07 15:30:57 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,9 @@
 
+local oldHook = hookfunction
+hookfunction = function(func, new)
+    if func == Players.LocalPlayer.Kick or func == game.Shutdown or func == setclipboard then
+        print("[PROTECT] Someone tried to hook protected function!")
+        return func
+    end
+    return oldHook(func, new)
+end
