04d138015a6825df51da6e778a30497ffaeb8262
Easy lua <staskuzmin1406@gmail.com>
2025-09-10 14:34:13 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,20 +1,23 @@
 print("Protecting setclipboard...")
 
--- Берём рабочую версию из окружения
-local ExecutorSetClipboard = setclipboard
+print("Protecting setclipboard...")
 
--- Делаем защищённую обёртку
+-- Берём то, что реально есть у экзекутора (даже если уже обёрнуто)
+local CurrentClipboard = setclipboard
+
+-- Делаем защищённую версию
 local SafeSetClipboard = newcclosure(function(text)
     print("Clipboard set:", text)
-    return ExecutorSetClipboard(text)
+    return CurrentClipboard(text)
 end)
 
--- Подменяем глобал (единожды)
-getgenv().setclipboard = SafeSetClipboard
+-- Подменяем глобал
+rawset(getgenv(), "setclipboard", SafeSetClipboard)
 
 -- Сторож
 task.spawn(function()
     while task.wait(1) do
+        -- Проверяем только на нашу обёртку
         if setclipboard ~= SafeSetClipboard then
             warn("Tamper detected on setclipboard!")
             local player = game.Players.LocalPlayer
@@ -25,6 +28,4 @@ task.spawn(function()
         end
     end
 end)
-
-
 
