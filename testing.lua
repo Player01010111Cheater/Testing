500f94243004b9f7c6ab112825768f72ad4b7f94
Easy lua <staskuzmin1406@gmail.com>
2025-09-10 16:58:52 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,12 +1,19 @@
-local raw_error = error
+local function checksum(str)
+    local sum = 0
+    for i = 1, #str do
+        sum = (sum + string.byte(str, i)) % 2^32
+    end
+    return sum
+end
+
+local raw_error = warn
 local hash_error = checksum(string.dump(raw_error))
 
 task.spawn(function()
     while true do
         if checksum(string.dump(error)) ~= hash_error then
-            raw_error("tamper detected")
+            raw_error("hook detected")
         end
         task.wait(1)
     end
 end)
-
