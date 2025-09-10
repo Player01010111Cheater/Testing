bcfe1157d64d4c3be2c6b7898b86a323b66399bb
Easy lua <staskuzmin1406@gmail.com>
2025-09-10 17:52:36 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,4 +1,18 @@
-local old
-old = hookfunction(warn, newcclosure(function (mesg)
-    return old(mesg)
-end))
+local originalError = error
+local safeError = function(message, level)
+    if _G.error ~= originalError then
+        warn("Обнаружена подмена функции error!")
+        return
+    end
+    originalError(message, level)
+end
+_G.error = safeError
+
+setmetatable(_G, {
+    __newindex = function(t, k, v)
+        if k == "error" then
+            warn("Попытка изменить функцию error!")
+        end
+        rawset(t, k, v)
+    end
+})
