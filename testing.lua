7d25627caefc3cbfd3611c53a71d0ee77245470a
Easy lua <staskuzmin1406@gmail.com>
2025-09-10 17:20:45 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,20 +1,10 @@
--- простая контрольная сумма
-local function checksum(str)
-    local sum = 0
-    for i = 1, #str do
-        sum = (sum + string.byte(str, i)) % 2^32
-    end
-    return sum
+-- Сохраняем локальную ссылку на оригинальный warn
+local raw_warn = warn
+
+-- Переопределяем глобальный warn в своём скрипте
+warn = function(msg)
+    raw_warn(msg) -- всегда вызывает оригинал через локальную ссылку
 end
 
-local raw_warn = newcclosure(warn) -- сохраняем оригинал
-local hash_warn = checksum(tostring(raw_warn))
-print(hash_warn)
-task.spawn(function()
-    while true do
-        if checksum(tostring(warn)) ~= hash_warn then
-            raw_warn("hook detected") -- срабатывает при hookfunction
-        end
-        task.wait(1)
-    end
-end)
+-- Теперь любой вызов warn("текст") будет использовать локальный raw_warn
+warn("Это сообщение невозможно перехватить hookfunction глобальной переменной")
